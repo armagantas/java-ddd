ebb903de5dc2a62583b6c5b73ade6340d7875257
armagantas <armagantas6@gmail.com>
2026-01-04 00:18:12 +0300

    implement events and business logic

--- order-service/order-domain/order-domain-core/src/main/java/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/domain/entity/Product.java
@@ -13,6 +13,11 @@ public class Product extends BaseEntity<ProductId> {
         this.name = name;
     }
 
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
---
+++ order-service/order-domain/order-domain-core/src/main/java/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,12 @@
+package domain.event;
+
+import domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent {
+
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
